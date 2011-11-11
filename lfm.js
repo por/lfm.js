999d66bf7b450cf607bd45059b3ad50213ec3b38
Tim <timbormans@gmail.com>
2011-11-11 13:21:16 +0100

    Add jslinting

--- lfm.js
+++ lfm.js
@@ -1,4 +1,4 @@
-window.LFM = window.LFM || (function() {
+window.LFM = window.LFM || (function () {
   var apiKey = null;
   var apiSecret = null;
   var apiRoot = 'http://ws.audioscrobbler.com/2.0/?format=json';
@@ -10,7 +10,7 @@ window.LFM = window.LFM || (function() {
   var authCallback = null;
 
   // Make an api request
-  var api = function(method, type, params, callback) {
+  var api = function (method, type, params, callback) {
     var data = {
       'api_key': apiKey,
       'method': method
@@ -36,48 +36,54 @@ window.LFM = window.LFM || (function() {
     }
     http_request.onreadystatechange = function () {
       var done = 4, ok = 200;
-      if (http_request.readyState == done && http_request.status == ok) {
+      if (http_request.readyState === done && http_request.status === ok) {
         callback(JSON.parse(http_request.responseText));
       }
     };
-    http_request.send((m==='POST')?data:null);
+    http_request.send((m === 'POST') ? data : null);
 
   };
 
   // Sign api request
-  var getApiSignature = function(params) {
+  var getApiSignature = function (params) {
     var keys   = [];
     var string = '';
-    for(var key in params){
-      keys.push(key);
+    var key, index, k;
+    for (key in params) {
+      if (params.hasOwnProperty(key)) {
+        keys.push(key);
+      }
     }
     keys.sort();
-    for(var index in keys){
-      var k = keys[index];
-      string += k + params[k];
+    for (index in keys) {
+      if (keys.hasOwnProperty(index)) {
+        k = keys[index];
+        string += k + params[k];
+      }
     }
     string += apiSecret;
-    return LFM.md5(string);
+    return hex_md5(string);
   };
 
   // Get/set data
   var Data = {
-    get: function(key) {
+    get: function (key) {
       return JSON.parse(localStorage.getItem(key));
     },
-    set: function(key, value) {
+    set: function (key, value) {
       localStorage.setItem(key, JSON.stringify(value));
     },
-    del: function(key) {
+    del: function (key) {
       localStorage.removeItem(key);
     }
   };
 
   // Create a serialized representation of an object
-  var parameterise = function(params){
+  var parameterise = function (params) {
     var a = [];
-    for(var name in params){
-      if(params.hasOwnProperty(name)){
+    var name;
+    for (name in params) {
+      if (params.hasOwnProperty(name)) {
         a.push(name + "=" + escape(params[name]));
       }
     }
@@ -85,36 +91,40 @@ window.LFM = window.LFM || (function() {
   };
 
   // Parse DOMWindow.location object
-  var parseLocation = function(location) {
+  var parseLocation = function (location) {
     var splitted = (location.search + location.hash).split(/[&?#]/);
     var obj = {};
-    for(var i in splitted){
-      var kv = splitted[i].split("=");
-      if(kv[0]){
-        obj[kv[0]] = unescape(kv[1]);
+    var i, kv;
+    for (i in splitted) {
+      if (splitted.hasOwnProperty(i)) {
+        kv = splitted[i].split("=");
+        if (kv[0]) {
+          obj[kv[0]] = unescape(kv[1]);
+        }
       }
     }
     return obj;
   };
   
   // Merge two objects
-  var mergeObjects = function(obj1,obj2) {
+  var mergeObjects = function (obj1, obj2) {
     var obj3 = {};
-    for (var attrname1 in obj1) { obj3[attrname1] = obj1[attrname1]; }
-    for (var attrname2 in obj2) { obj3[attrname2] = obj2[attrname2]; }
+    var attrname1, attrname2;
+    for (attrname1 in obj1) { if (obj1.hasOwnProperty(attrname1)) { obj3[attrname1] = obj1[attrname1]; } }
+    for (attrname2 in obj2) { if (obj2.hasOwnProperty(attrname2)) { obj3[attrname2] = obj2[attrname2]; } }
     return obj3;
   };
 
   return {
-    init: function(options) {
+    init: function (options) {
       apiKey = options.key;
       apiSecret = options.secret || null;
       session = Data.get('session');
     },
-    getSession: function() {
+    getSession: function () {
       return session;
     },
-    login: function(callback) {
+    login: function (callback) {
       authCallback = callback;
       var url = apiAuthUrl + '?' + parameterise({
         api_key: apiKey,
@@ -122,17 +132,17 @@ window.LFM = window.LFM || (function() {
       });
       window.open(url, 'lastfmLogin', 'height=600,width=980');
     },
-    logout: function(callback) {
+    logout: function (callback) {
       Data.del('session');
       callback();
     },
-    loginCallback: function() {
+    loginCallback: function () {
       var popup = window.open("", "lastfmLogin");
       var params = parseLocation(popup.location);
       popup.close();
       if (params.token) {
         // get session
-        api('auth.getSession', 'post', {token: params.token}, function(response) {
+        api('auth.getSession', 'post', {token: params.token}, function (response) {
           Data.set('session', response.session);
         });
       }
--- makefile
+++ makefile
@@ -1,8 +1,13 @@
 # Set the default files to be built
 default: lfm.min.js
 
+# Install nodejs dependencies
+install:
+	npm install jslint
+	npm install nopt
+
 # Compress lfm.js into lfm.min.js
 lfm.min.js: lfm.js
 	@@echo 'Compressing lfm.min.js...'
-	@@java -jar build/compiler.jar --js $^ --js_output_file $@
+	@@java -jar lib/compiler.jar --js $^ --js_output_file $@
 	@@echo 'Done!'
